a956ebd44bd64398db3b9af6ac8082f21dd68d28
Sanket Singh <singhsanket143@gmail.com>
2024-09-29 11:17:40 +0530

    Added codes

--- driver.js
+++ driver.js
@@ -1,3 +1,20 @@
-const { linearSearch, binarySearch } = require('./searching.js');
+// const { linearSearch, binarySearch } = require('./searching.js');
 
-console.log(linearSearch);
+// console.log(linearSearch);
+
+const fs = require('fs/promises');
+
+async function readSampleFile() {
+    const data = await fs.readFile('./sample.txt', 'utf-8');
+    console.log(data);
+}
+
+async function writeSampleFile() {
+    await fs.writeFile('./sample.txt', 'Hello World');
+}
+
+async function appendSampleFile() {
+    await fs.appendFile('./sample.txt', 'Hello World');
+}
+
+appendSampleFile();
